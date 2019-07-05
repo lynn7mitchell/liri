d191a7f29574339e7d6ea66e00e2d81de0d0c776
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-07-05 17:02:16 -0500

    Can get response from OMDB

--- liri.js
+++ liri.js
@@ -9,6 +9,20 @@ const inquirer = require('inquirer');
 const moment = require("moment");
 
 
+// OMDB REQUEST
 
+axios.get('http://www.omdbapi.com/?apikey='+ omdb + '&t=anchorman'  )
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+ 
 
 
