9536a91131d60eb45fa3cbe5cd9ef3918cdb99d9
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-07-06 08:17:27 -0500

    OMDB function can get an object from omdb

--- liri.js
+++ liri.js
@@ -1,17 +1,48 @@
 require("dotenv").config();
 
 const keys = require('./keys.js');
-const Spotify = new Spotify(keys.spotify);
+// const Spotify = new Spotify(keys.spotify);
 const omdb = keys.omdb.key;
 
 const axios = require("axios");
 const inquirer = require('inquirer');
 const moment = require("moment");
 
+//USER INPUT VARIABLES
+let userCommand = process.argv[2];
+let userInput = process.argv[3];
+
+// let userInputArr = [];
+// let userInput = userInputArr.toString();
+// for(let i = 2; i < process.argv.length; i++){
+//     if(process.argv[i] != null){
+//     userInputArr.push(process.argv[i]);
+//     }
+//   }
+// console.log(userInputArr)
+// console.log(userInput)
+
+switch(userCommand){
+  case "concert-this":
+    console.log("worked");
+    break
+  case "spotify-this-song":
+    console.log(2);
+    break
+  case "movie-this":
+    omdb();
+    break
+  case"do-what-it-says":
+    console.log(4);
+    break
+}
+
+
 
 // OMDB REQUEST
 
-axios.get('http://www.omdbapi.com/?apikey='+ omdb + '&t=anchorman'  )
+var omdb = function(){
+  axios.get('http://www.omdbapi.com/?apikey='+ userInput + '&t=anchorman'  )
   .then(function (response) {
     // handle success
     console.log(response);
@@ -23,6 +54,6 @@ axios.get('http://www.omdbapi.com/?apikey='+ omdb + '&t=anchorman'  )
   .finally(function () {
     // always executed
   });
- 
+}
 
 
